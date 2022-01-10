25d154b842bc3e7ecb1cd3e135cc8fde80b4d619
Quang Nha <quangnha92@gmail.com>
2022-01-11 03:44:30 +0700

    fix loi moi

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-    }//tiep tuc them moi
+    }
 
     public static void main(String[] args) {
         launch();
